fcd79373615ae5f97a37d4c214833b058f6dd383
YaizaCIT <56362574+YaizaCIT@users.noreply.github.com>
2019-11-27 12:46:17 +0000

    V1
    Commit 1 to start game in python

--- HelloPython.py
+++ HelloPython.py
@@ -1,4 +1,12 @@
 import datetime
+import time
+
+#welcoming the user
+name = raw_input("What is your name? ")
+print "Hello, " + name, "Time to play hangman!"
+print "
+"
+
 print("Hello World")
 now = datetime.datetime.now()
 print("Current date and time is ")
